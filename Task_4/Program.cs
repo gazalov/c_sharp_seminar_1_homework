3a23bba2bcac57205fb71173a5c1b078ebbb5c64
a.gazalov <gazalov@list.ru>
2022-11-22 10:19:33 +0300

    Task 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите число 1: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int numberB = int.Parse(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int numberC = int.Parse(Console.ReadLine());
+
+int max = numberA;
+if(numberB > max)
+{
+    max = numberB;
+}
+if(numberC > max)
+{
+    max = numberC;
+}
+Console.Write($"Максимаоьное число: {max}");
