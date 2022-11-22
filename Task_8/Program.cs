26a7db2ea36ee79a7941b7644f5781bff87754e5
a.gazalov <gazalov@list.ru>
2022-11-22 11:43:56 +0300

    Task 8 Even numbers fm 1 to N

---
+++ Task_8/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+//It is impossible to use divider in noninteger numbers as "," so decided to chanche localisation to en-US to use "."
+Console.Clear();
+Console.Write("Введите целое положительное число: ");
+
+int numberA = int.Parse(Console.ReadLine());
+int i = 1;
+Console.WriteLine($"Вывожу положительные числа от 1 до {numberA}:");
+while(i<=numberA)
+{
+    if(i % 2 == 0) 
+    {
+    Console.WriteLine(i);
+    }
+i++;
+}
