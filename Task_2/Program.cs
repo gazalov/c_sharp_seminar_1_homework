b870c3545554ac4f0eb80ab7094020f1a200f28d
a.gazalov <gazalov@list.ru>
2022-11-22 10:10:21 +0300

    Task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число 1: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if(numberA>numberB)
+{
+Console.Write("Первое число больше второго.");
+}
+else
+{
+Console.Write("Второе число больше первого.");
+}
