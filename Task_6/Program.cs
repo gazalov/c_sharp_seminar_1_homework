a48b744ffc5117ec74c332341226fe7153c7a0b0
a.gazalov <gazalov@list.ru>
2022-11-22 11:27:43 +0300

    Task 6 Even number verifying

---
+++ Task_6/Program.cs
@@ -0,0 +1,14 @@
+﻿System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+//It is impossible to use divider in noninteger numbers as "," so decided to chanche localisation to en-US to use "."
+Console.Clear();
+Console.Write("Введите число: ");
+
+double numberA = double.Parse(Console.ReadLine());
+double numberB = numberA % 2;
+if(numberA % 2 == 0) 
+{
+    Console.WriteLine($"Число {numberA} является четным числом.");
+}
+else {
+    Console.WriteLine($"Число {numberA} является нечетным числом.");
+}
